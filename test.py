f30ca2880a2e4921e0cb25cc68a83394508886de
VIBHOR RAWAL <46536580+vibhorrawal@users.noreply.github.com>
2019-06-07 23:37:57 +0530

    deleting testing file

--- test.py
+++
@@ -1,9 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description='You can add a description here')
-parser.add_argument('-n','--name', help='Your name',required=False)
-parser.add_argument('-q', '--query')
-args = parser.parse_args()
-
-
-if __name__ == '__main__':
-	print (args.name, args.query)
